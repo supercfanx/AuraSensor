33874d02c1ec91ef73622505d76afc0f237e2ed6
Shenwei Liu <supercfanx@gmail.com>
2012-02-14 14:50:11 -0500

    1. Refactored the code 2. Add keyboard shortcut 'q' to quit 3. Add framerate report

--- matlab/visualize.m
+++ matlab/visualize.m
@@ -1,51 +1,105 @@
+function visualize
+
+
+nrow = 8;
+ncolumn = 22;
+calibrationFrame = 10;
+offsetImage = zeros(ncolumn, nrow);
+running = true;
+
+
+    function synchronizeSerial
+        disp('Synchronizing...')
+        while (ser.BytesAvailable <= 0)
+            pollSerial();
+            pause(0.001);
+        end
+    end
+
+    function pollSerial
+        fwrite(ser, [255], 'uint8');
+    end
+
+    function img = readImage
+        pollSerial();
+        img = fread(ser, [ncolumn, nrow], 'uint16');
+    end
+
+    function calibrate
+        disp('Calibrating...')
+        offsetImage = zeros(ncolumn, nrow);
+        for ci=1:calibrationFrame
+            offsetImage = offsetImage + readImage();
+        end
+        offsetImage = offsetImage ./ calibrationFrame;
+    end
+
+    function pimg = processImage(img)
+        pimg = min(2047, max(0, img - offsetImage));
+        pimg = uint8(pimg .^ 0.5 .* 5);
+        pimg = imrotate(pimg, 90);
+    end
+
+    function onKeyPress(source, eventData)
+        if eventData.Character == 'q'
+            running = false;
+        end
+    end
+
+ser = openSerial();
+synchronizeSerial();
+calibrate();
+
+%failindice = [];
+%sumimage = zeros(ncolumn, nrow);
+
+ih = figure;
+set(ih, 'WindowKeyPressFcn', @onKeyPress)
+frameCnt = 0;
+
+disp('Running...')
+figure(ih);
+tic
+try
+    while true
+        if ~running
+            break;
+        end
+        rawimg = readImage();
+        image = processImage(rawimg);
+        imshow(image);
+        %figure(ih), surf(1:nrow, 1:ncolumn, image);
+        drawnow
+        frameCnt = frameCnt + 1;
+        %sumimage = sumimage + image;
+        %failindice = intersect(failindice, find(image<500));
+    end
+catch err
+    closeSerial(ser);
+    rethrow(err);
+end
+
+disp(sprintf('Framerate: %.1f', frameCnt / toc()));
+
+%sumimage = sumimage ./ runFrame;
+
+%ind2sub(size(image), failindice)
+closeSerial(ser);
+
+end
+
+function s = openSerial
 info = instrhwinfo('serial');
 available_ports = info.AvailableSerialPorts
-
-row = 8;
-column = 22;
-
 %s = serial(available_ports(1));
 s = serial('/dev/tty.usbserial-A900UD1B');
 set(s, 'BaudRate',230400)
 set(s, 'InputBufferSize', 50000);
 fopen(s);
-
-failindice = [];
-
-image = uint16(zeros(column, row));
-sumimage = zeros(column, row);
-
-while (s.BytesAvailable <= 0) 
-    fwrite(s, [255], 'uint8');
-    pause(0.01);
 end
 
-calibrationFrame = 10;
-offsetImage = zeros(column, row);
-for i=1:calibrationFrame
-   fwrite(s, [255], 'uint8');
-   offsetImage = offsetImage + fread(s, [column, row], 'uint16'); 
-end
-offsetImage = offsetImage ./ calibrationFrame;
-
-for i = 1:600
-   fwrite(s, [255], 'uint8');
-   rawimage = fread(s, [column, row], 'uint16'); 
-   image = min(2047, max(0, rawimage - offsetImage));
-   %image = image .^ 0.333 .* 20;
-   image = image .^ 0.5 .* 5;
-   %sumimage = sumimage + image;
-   %failindice = intersect(failindice, find(image<500));
-   %surf(1:14, 1:8, image);
-   imshow(uint8(image));
-   drawnow
-end
-
-%sumimage = sumimage ./ 600;
-%hist(sumimage(:), 0:10:10000);
-
-
-%ind2sub(size(image), failindice)
-
+function closeSerial(s) 
 fclose(s);
 delete(s);
+end
+
